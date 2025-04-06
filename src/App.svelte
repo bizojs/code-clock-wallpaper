e99bd6a4e039517d053e2fb3d36b4ad4338d28a4
Brad <44570552+bizojs@users.noreply.github.com>
2025-04-06 19:31:44 +0100

    feat: publish to workshop
    refactor: change preview image (wouldnt upload with gif)

--- src/App.svelte
+++ src/App.svelte
@@ -25,15 +25,23 @@
 
 <div class="flex w-screen h-screen relative">
     <ToggleButton bind:hidden />
-    {#if $backgroundImage?.value ?? true}
-        <img transition:fade src="wallpaper.jpg" alt="" class="z-[-2] fixed top-0 left-0 w-full h-full select-none pointer-events-none">
+    {#if $backgroundImage?.value ?? false}
+        <img
+            transition:fade
+            src="wallpaper.jpg"
+            alt=""
+            class="z-[-2] fixed top-0 left-0 w-full h-full select-none pointer-events-none"
+        >
         <div class={cn(
             "z-[-1] fixed top-0 left-0 w-full h-full shadow-lg select-none pointer-events-none transition-all duration-1000",
             hidden ? "bg-background/0 backdrop-blur-none" : "bg-background/60 backdrop-blur-2xl"
         )}></div>
     {/if}
     {#if !hidden}
-        <div transition:fly={{ y: -60, duration: 800, easing: cubicInOut }} class="flex flex-col w-fit fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[999]">
+        <div
+            transition:fly={{ y: -60, duration: 800, easing: cubicInOut }}
+            class="flex flex-col w-fit fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[999]"
+        >
 <pre class="{$theme?.value ?? "theme-blueberry"} transition-colors duration-700 drop-shadow-xl select-none text-2xl text-misc"><Const value="const" /> <Variable value="time" />: <Type value="Time" /> = {"{"}
     <Quote /><String value="hour" /><Quote />    : <Int bind:value={data.hour} />,
     <Quote /><String value="minute" /><Quote />  : <Int bind:value={data.minute} />,
